38fcec80879b8f02dc52efc358d7069913012762
Aleksandr Maryasov <ivanmontun@gmail.com>
2020-04-01 01:06:51 +0300

    Создал файл скрипта и подключил его, написал скрипт

---
+++ script/script.js
@@ -0,0 +1,11 @@
+let money = 5,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period;
+
+const commit = document.querySelector('.new_commit')
+console.log('commit: ', commit);
+
+alert(money);
