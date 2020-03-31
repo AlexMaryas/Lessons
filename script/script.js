7adb51481806578fe7bb91bd3bcfbce5c2af4635
Aleksandr Maryasov <ivanmontun@gmail.com>
2020-04-01 02:08:34 +0300

    Поменял скрипт, выполнил 2домашнее задание

--- script/script.js
+++ script/script.js
@@ -1,11 +1,24 @@
-let money = 5,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
-
-const commit = document.querySelector('.new_commit')
-console.log('commit: ', commit);
-
-alert(money);
+let money = 100,
+    income = 'Казино',
+    addExpenses = 'интернет, такси, коммуналка',
+    deposit = true,
+    mission = 500000,
+    period = 12,
+    budgetDay = money/30;
+    
+    const commit = document.querySelector('.new_commit')
+    console.log('commit: ', commit);
+    
+    alert(money);
+    
+    console.log(typeof money);
+    console.log(typeof income);
+    console.log(typeof deposit);
+    console.log(addExpenses.length);
+    console.log(`Период равен ${period} месяцев`);
+    console.log(`Цель заработать ${mission} рублей`);
+    
+    console.log('addExpenses.toLowerCase().split(', '): ', addExpenses.toLowerCase().split(', '));
+    console.log('budgetDay: ', budgetDay);
+    
+    
