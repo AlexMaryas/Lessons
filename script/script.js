e1790ce2802a96c46e994aeb1e3ea14dfb1cc4cf
Aleksandr Maryasov <ivanmontun@gmail.com>
2020-04-05 21:17:16 +0300

    Выполнил 6 ДЗ

--- script/script.js
+++ script/script.js
@@ -1,9 +1,27 @@
-
-let randomizer = function(max, min) {
-    return Math.trunc(Math.random() * max) + min;
-};
-let randNum = randomizer(100, 1);
-
-
-
-
+function game() {
+    let randomizer = function(max, min) {
+        return Math.trunc(Math.random() * max) + min;
+    };
+    let randNum = randomizer(100, 1);
+    console.log(randNum);
+    let userInt = function() {
+        let userNumber = prompt('Угадай число от 1 до 100', 100);
+        if (userNumber === null) {
+            alert('Bye-bye!');
+            return;
+        }else if (userNumber > randNum) {
+            alert('Загаданное число меньше');
+        } else if (userNumber < randNum) {
+            alert('Загаданное число больше');
+        } else if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
+            alert(' Введи число от 1 до 100');
+        } else if (+userNumber === +randNum) {
+            alert('Вы победили!');
+            return;
+        }
+        userInt();
+    };
+    console.dir(userInt);
+    userInt();
+}
+game();
