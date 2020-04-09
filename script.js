6baa7b6096243e853e5ab9b9277527c51227f4a5
Aleksandr Maryasov <ivanmontun@gmail.com>
2020-04-10 00:34:28 +0300

    Восстановил порядок глав во второй и пятой книге

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const books = document.querySelectorAll('.book'),
       booksH2A = document.querySelectorAll('.book>h2>a'),
       adv = document.querySelector('.adv'),
+      chapters = document.querySelectorAll('.book>ul>li'),
       book1 = books[1],
       book2 = books[0],
       book3 = books[4],
@@ -15,3 +16,13 @@ book5.after(book6);
 document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 booksH2A[4].textContent = 'Книга 3. this и Прототипы Объектов';
 adv.remove();
+
+chapters[9].after(chapters[2]);
+chapters[8].after(chapters[7]);
+chapters[8].after(chapters[4]);
+chapters[4].after(chapters[5]);
+
+chapters[56].before(chapters[54]);
+chapters[54].before(chapters[51]);
+chapters[47].after(chapters[55]);
+chapters[50].after(chapters[48]);
