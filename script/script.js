32f2919d7a114d37ad52769b25cf3020bcb2ba5d
Aleksandr Maryasov <ivanmontun@gmail.com>
2020-04-17 20:01:51 +0300

    Почти всё равботает. Осталось запилить добавление удаление после нажатия на кнопки "удалить" и "добавиnь в выполненные"

---
+++ script/script.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control'),
+      headerInput = document.querySelector('.header-input'),
+      todoList = document.querySelector('.todo-list'),
+      todoCompleted = document.querySelector('.todo-completed');
+
+let todoData = [{kj:1}];
+
+let obj = [0, 'dfdf']
+const render = function () {
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+    console.log('todoData: ', todoData);
+    todoData.splice(0);
+    console.log('todoData: ', todoData);
+    let returnTodoData = JSON.parse(localStorage.getItem('todoData'));
+    console.log('returnTodoData: ', returnTodoData);
+    todoData = todoData.concat(returnTodoData);
+    console.log('todoDataBEFORE: ', todoData);
+    
+    
+
+    todoData.forEach(function (item,i) {
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+        '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+        '</div>';
+        
+        if (item.complited) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+        
+        const btnTodoComplete = li.querySelector('.todo-complete');
+        btnTodoComplete.addEventListener('click', function () {
+            item.complited = !item.complited;
+            render();
+        });
+        const btnDelete = li.querySelector('.todo-remove');
+        btnDelete.addEventListener('click', function (event) {
+                    todoData.splice(i, 1);
+                    event.target.parentNode.parentNode.remove();
+        });
+    });
+};
+
+todoControl.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const newTodo = {
+        value: headerInput.value,
+        complited: false
+    };
+    if (newTodo.value !== '') {
+    todoData.push(newTodo);
+
+    let serialTodoData = JSON.stringify(todoData);
+    console.log('serialTodoData: ', serialTodoData);
+    localStorage.setItem("todoData",serialTodoData);
+    
+    render();
+    headerInput.value = '';
+    }
+
+});
+if(localStorage.getItem('obj')) {
+todoData.push(localStorage.getItem('obj'));
+}
+render();
+let a = [
+    {
+        aa: 'aaa',
+        aa1: 1
+    }
+
+];
+let b = [
+    {
+        bb: 'bbb',
+        bb1: 1
+    },
+    {
+        bc: 'bcb',
+        bcb: 2
+    }
+
+];
+a = a.concat(b);
+console.log(a);
