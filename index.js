779b392a019ae5c77f340ae55b1c9de7749f38ee
Aleksandr Maryasov <ivanmontun@gmail.com>
2020-05-13 13:18:40 +0300

    Lesson04H

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-//1пункт
-const weekDaysRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    weekDaysEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-    weekDays = [weekDaysRu, weekDaysEn];
-    question = prompt('Choose language: "ru"-русский или "en"-english', 'ru'),
-    question2 = prompt('Choose language: 0 - русский, 1 - english', 0);
 const result = (value) => {
-    value.forEach(element => {
-        console.log(element);
-    });
+    if (typeof value != 'string') {
+        alert(`It's not a string: ${value}`);
+    } else {
+        value = value.trim();
+        if (value.length > 30) {
+            value.length === 30;
+            value = value + "...";
+        }
+        console.log(value);
+    }
 };
-//a
-if (question === 'ru') {
-    result(weekDaysRu);
-} else if (question === 'en') {
-    result(weekDaysEn);
-} 
-//b
-switch(question) {
-    case 'ru':
-        result(weekDaysRu);
-    case 'en':
-        result(weekDaysEn);
-}
-//c
-result(weekDays.question2);
-//2пункт
-const namePerson = prompt('Введите свое имя: ');
-
-namePerson === 'Артем' ? console.log('Директор') : namePerson === 'Максим' ? 
-    console.log('преподаватель') : console.log('студент');
+result('                   dqqqqqqqqqqqqqqqqq12345678901212098765-            ');
