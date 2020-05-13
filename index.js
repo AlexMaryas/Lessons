c69a9fc4bc6ee113820caa704ed28d963848fe8c
Aleksandr Maryasov <ivanmontun@gmail.com>
2020-05-13 13:49:07 +0300

    Lesson05H complited

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const result = (value) => {
-    if (typeof value != 'string') {
-        alert(`It's not a string: ${value}`);
-    } else {
-        value = value.trim();
-        if (value.length > 30) {
-            value.length === 30;
-            value = value + "...";
-        }
-        console.log(value);
+const arr = ['123', '456', '789', '5000', '8452', '81', '2548601'];
+
+arr.forEach(element => {
+    if (element[0] === '4' || element[0] === '2') {
+        console.log(element);
     }
-};
-result('                   dqqqqqqqqqqqqqqqqq12345678901212098765-            ');
+});
+
+for (let i = 2; i <= 100; i++) {
+    let count = 0;
+    for (let j = 2; j < i; j++) {
+        if ( i % j === 0) {
+            count++;
+            break;
+        }
+    }
+    if (count === 0) {
+        console.log(`${i}. Делители этого числа: 1 и ${i}`);
+    }
+}
