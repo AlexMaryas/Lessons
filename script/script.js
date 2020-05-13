ac1522c0dc3da7f4158bcf898202f59beab766f6
Aleksandr Maryasov <ivanmontun@gmail.com>
2020-05-13 19:35:21 +0300

    Lesson06H completed

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
+let randomizer = function(max, min) {
+    return Math.trunc(Math.random() * max) + min;
+};
 function game() {
-    let randomizer = function(max, min) {
-        return Math.trunc(Math.random() * max) + min;
-    };
+    let count = 10;
     let randNum = randomizer(100, 1);
     console.log(randNum);
     let userInt = function() {
@@ -9,19 +10,35 @@ function game() {
         if (userNumber === null) {
             alert('Bye-bye!');
             return;
-        }else if (userNumber > randNum) {
-            alert('Загаданное число меньше');
-        } else if (userNumber < randNum) {
-            alert('Загаданное число больше');
+        }else if ( count === 0 ) {
+            const question = confirm('К сожалению, попытки закончились! Хотите сыграть ещё?');
+            if (question) {
+                game();
+            } else {
+                alert('bye-bye');
+                return;
+            }
+        }else if (count > 0 && userNumber > randNum) {
+            count--;
+            alert(`Загаданное число меньше, число оставшихся попыток: ${count}`);
+            userInt();
+        } else if (count > 0 &&  userNumber < randNum) {
+            count--;
+            alert(`Загаданное число больше, число оставшихся попыток: ${count}`);
+            userInt();
         } else if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
             alert(' Введи число от 1 до 100');
+            userInt();
         } else if (+userNumber === +randNum) {
-            alert('Вы победили!');
-            return;
+            const question = confirm('Вы победили! Хотите сыграть ещё?');
+            if (question) {
+                game();
+            } else {
+                alert('bye-bye');
+                return;
+            }
         }
-        userInt();
     };
-    console.dir(userInt);
     userInt();
 }
-game();
+ game(); 
